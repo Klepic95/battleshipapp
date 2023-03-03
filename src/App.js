7b867495c40bbd1ef80e1404f82acf798d12a833
Klepic95 <klepicpredrag@gmail.com>
2023-03-03 01:31:26 +0100

    Light switch added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import ReactSwitch from "react-switch";
 import "./App.css";
 
 // Components
@@ -7,9 +8,17 @@ import Board from "./components/Board";
 import Footer from "./components/Footer";
 
 function App() {
+  const [checked, setChecked] = useState(false);
+  const handleCheck = (val) => {
+    setChecked(val);
+  };
   return (
-    <div>
+    <div style={{ backgroundColor: checked ? "#696969" : "#f0ffff" }}>
       <Header title="Battleship Game" />
+      <div className="App">
+        <ReactSwitch checked={checked} onChange={handleCheck} />
+        <p>Dark Mode</p>
+      </div>
       <Board />
       <Footer />
     </div>
